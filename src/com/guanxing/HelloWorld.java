a14ae37ba82f677b0f1ace72c24b69ec452d688b
guanxing <15813329880@163.com>
2021-04-12 14:29:08 +0800

    hello world

---
+++ src/com/guanxing/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.guanxing;
+
+/**
+ * @author guanxing
+ * @version 1.11
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
